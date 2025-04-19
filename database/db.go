3ef6286e9b2b823c8918322d4669186e714f8c4d
muna <munah57@live.com>
2025-04-19 14:20:07 +0100

    db instance created

--- database/db.go
+++ database/db.go
@@ -1,2 +1,40 @@
 package db
 
+import (
+	"fmt"
+	"context"
+	"log"
+	"time"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDB := "mongodb://localhost:27017"
+	fmt.Println(MongoDB)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err !=nil {
+		log.Fatal(err)
+	}
+	return client
+	
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection (client *mongo.Client, collecttionName string) *mongo.Collection {
+	
+	var collection *mongo.Collection = client.Database("real_chat").Collection(collecttionName)
+	fmt.Println("collection opened")
+	
+	return collection
+}
