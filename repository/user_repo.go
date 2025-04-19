9c2f532bf4affdbf409e717d8a5c6272dc9e6dd7
muna <munah57@live.com>
2025-04-19 20:26:31 +0100

    update  GetPaginatedUsers

--- repository/user_repo.go
+++ repository/user_repo.go
@@ -5,6 +5,7 @@ import (
 	"real-chat/models"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -20,9 +21,35 @@ func NewUserRepository(collection *mongo.Collection) UserRepository {
 	return &UserRepo{collection: collection}
 }
 
-func (r *UserRepo) GetPaginatedUsers(ctx context.Context, recordsPerPage, page int) ([]models.User, error) {
+type PaginatedUserResult struct {
+	TotalCount int          `bson:"total_count"`
+	Users      []models.User `bson:"user_items"`
+}
+
+func (r *UserRepo) GetPaginatedUsers(parentCtx context.Context, recordsPerPage, page int) ([]models.User, error) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
+	startIndex := (page - 1) * recordsPerPage
+
+	matchStage := bson.D{{Key: "$match", Value: bson.D{}}}
+	projectStage := bson.D{
+		{Key: "_id", Value: 0},
+		{Key: "total_count", Value: 1},
+		{Key: "user_items", Value: bson.D{{Key: "$slice", Value: bson.A{"$data", startIndex, recordsPerPage}}}},
+	}
+
+	result, err := r.collection.Aggregate(ctx, mongo.Pipeline{matchStage, projectStage})
+	if err != nil {
+		return nil, err
+	}
+
+	var results []PaginatedUserResult
+	if err = result.All(ctx, &results); err != nil {
+		return nil, err
+	}
+
+	return results[0].Users, nil
 
 }
